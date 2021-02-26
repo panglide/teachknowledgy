f0cf198a300d3f49e6ba35e9dd6f1773298aaa49
Phillip Glidewell <panthonyglidewell@gmail.com>
2021-02-26 03:11:39 -0600

    Path

--- app/Http/Controllers/StandardController.php
+++ app/Http/Controllers/StandardController.php
@@ -37,11 +37,9 @@ class StandardController extends Controller
       
    
     $local_path = '/usr/local/bin/pdftotext';
-    $remote_path = '/vendor/spatie/pdf-to-text';
+    $remote_path = '/usr/local/sbin/pdftotext';
     //Read PDF and extract text
-    // $data = Pdf::getText('../public/'.$filename, $remote_path);
-
-    $data = (new Pdf())->setPdf('../public/'.$filename)->text();
+    $data = Pdf::getText('../public/'.$filename, $remote_path);
     
     // Parse out PDF  
     $standards_arrays = preg_split('/([1-8]\.[A-Z]{1,3}\.[A-Z]{1}\.\d)/', $data, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY );
