d937aa44ab8f2b84d4191f1660e5fa4dfbf8a9c0
Phillip Glidewell <panthonyglidewell@gmail.com>
2021-02-26 03:38:35 -0600

    Path

--- resources/views/standards/index.blade.php
+++ resources/views/standards/index.blade.php
@@ -4,7 +4,7 @@
 <div>Thank you for trying Teachknowledgy!</div>
 <div>We are gathering your materials now.</div>
 
-<a href="{{ url('') }}"></a><button class="btn btn-outline-secondary">View Dashboard</button>
+<a href="{{ url('/dashboard') }}"></a><button class="btn btn-outline-secondary">View Dashboard</button>
 
 
 @endsection
