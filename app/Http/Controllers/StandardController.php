db84e13b983759b8df65ae8a3f4884a89bc24675
Phillip Glidewell <panthonyglidewell@gmail.com>
2021-02-26 03:26:28 -0600

    Path

--- app/Http/Controllers/StandardController.php
+++ app/Http/Controllers/StandardController.php
@@ -37,7 +37,7 @@ class StandardController extends Controller
       
    
     $local_path = '/usr/local/bin/pdftotext';
-    $remote_path = '/usr/vendor/bin/pdftotext';
+    $remote_path = '/usr/sbin/pdftotext';
     //Read PDF and extract text
     $data = Pdf::getText('../public/'.$filename, $remote_path);
     
